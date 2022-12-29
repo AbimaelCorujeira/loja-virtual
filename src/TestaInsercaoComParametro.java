ee7aba28a03a7cf4e0d36ccd9fd8f9d1d06daa46
AbimaelCorujeira <corujeira.abimael@gmail.com>
2022-12-29 17:52:55 -0300

    commit e rollback

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -5,24 +5,30 @@ public class TestaInsercaoComParametro {
 
         ConnectionFactory connectionFactory = new ConnectionFactory();
         Connection connection = connectionFactory.criaConexao();
+        connection.setAutoCommit(false);
 
-        PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao)"
-                + "VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
-
-        adicionarVariavel("SmartTV", "60 polegadas", stm);
-        adicionarVariavel("Toca Discos", "acionado por correia", stm);
+        try {
+            PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao)"
+                    + "VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
+            adicionarVariavel("SmartTV", "60 polegadas", stm);
+            adicionarVariavel("Toca Discos", "acionado por correia", stm);
+            connection.commit();
+            stm.close();
+            connection.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("ROLLBACK REALIZADO");
+            connection.rollback();
+        }
     }
 
     private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
         stm.setString(1, nome);
         stm.setString(2, descricao);
-
-        if(nome.equals("Toca Discos")) {
+        if(nome.equals("")) {
             throw new RuntimeException("Não foi possivel adicionar o produto");
         }
-
         stm.execute();
-
         ResultSet resultSet = stm.getGeneratedKeys();
         while(resultSet.next()) {
             Integer id = resultSet.getInt(1);
